e4afad8f1bc590299aad298bfde8ae7893bb855e
abrahumlink <307887491@qq.com>
2022-02-11 16:58:30 +0800

    add v11

---
+++ src/parse/mod.rs
@@ -0,0 +1,12 @@
+use async_trait::async_trait;
+mod v12;
+mod v11;
+
+pub(crate) trait Parse<T> {
+    fn parse(self) -> T;
+}
+
+#[async_trait]
+pub(crate) trait Parser<X, Y> {
+    async fn parse(&self, input: X) -> Option<Y>;
+}
